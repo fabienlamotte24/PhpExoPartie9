e2b9addbe73418ea338aad6bde48c114c3e16daf
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-08-21 15:22:50 +0200

    Exo1 partie 9 PHP

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <title>Exercice 1</title>
+  </head>
+  <body>
+      <div class="container">
+          <div class="row">
+              <div class="col-12">
+                  <!--Nous créons une valeur $date dans lequel nous demandons une date sans donnée
+                  qui viendra selectionner la date d'aujourd'hui par défaut
+                  Puis l'on demande à l'afficher sous le format jj/mm/aaaa-->
+                  <?php 
+                  $date = new DateTime();
+                  echo $date->format('D/M/Y');
+                  ?>
+              </div>
+          </div>
+      </div>
+  </body>
+</html>
